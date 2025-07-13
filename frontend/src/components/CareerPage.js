1b34c4d0879dea368c4ef89abc9e745285704a25
Sameeksha Gupta <sameeksha.gce22@sot.pdpu.ac.in>
2025-07-13 13:48:19 +0530

    final

--- frontend/src/components/CareerPage.js
+++ frontend/src/components/CareerPage.js
@@ -669,7 +669,7 @@ const CareerPage = () => {
                   disabled={isSubmitting}
                   style={checkboxStyle}
                 />
-                <span>Ii consent to the processing of my personal data *</span>
+                <span>I consent to the processing of my personal data *</span>
               </label>
             </motion.div>
 
